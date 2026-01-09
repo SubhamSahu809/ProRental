2f495c298a34501719d3686576233f29a0a821c8
SubhamSahu809 <subhamkumarsahu896@gmail.com>
2026-01-09 11:15:52 +0530

    import issue

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,9 +1,42 @@
-import React from "react";
-import Login from "../components/Login";
+import React, { useState } from "react";
+import LoginModal from "../components/LoginModal";
+import SignupModal from "../components/SignupModal";
 
 const LoginPage = () => {
+  const [isLoginModalOpen, setIsLoginModalOpen] = useState(true);
+  const [isSignupModalOpen, setIsSignupModalOpen] = useState(false);
+
+  const closeLoginModal = () => {
+    setIsLoginModalOpen(false);
+  };
+
+  const closeSignupModal = () => {
+    setIsSignupModalOpen(false);
+  };
+
+  const switchToSignup = () => {
+    setIsLoginModalOpen(false);
+    setIsSignupModalOpen(true);
+  };
+
+  const switchToLogin = () => {
+    setIsSignupModalOpen(false);
+    setIsLoginModalOpen(true);
+  };
+
   return (
-    <Login />
+    <>
+      <LoginModal 
+        isOpen={isLoginModalOpen} 
+        onClose={closeLoginModal} 
+        onSwitchToSignup={switchToSignup}
+      />
+      <SignupModal 
+        isOpen={isSignupModalOpen} 
+        onClose={closeSignupModal}
+        onSwitchToLogin={switchToLogin}
+      />
+    </>
   );
 };
 
